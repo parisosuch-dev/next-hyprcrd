513f28d985b1b178e61d38f0258aec1baf70521d
parisosuch-dev <parisosuch.dev@gmail.com>
2024-01-25 20:33:13 -0800

    Add krypton font to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,27 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const krypton = localFont({
+  src: [
+    {
+      path: "../public/fonts/MonaspaceKrypton-Regular.otf",
+      weight: "400",
+    },
+    {
+      path: "../public/fonts/MonaspaceKrypton-Bold.otf",
+      weight: "700",
+    },
+    {
+      path: "../public/fonts/MonaspaceKrypton-ExtraBold.otf",
+      weight: "800",
+    },
+  ],
+  variable: "--font-krypton",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +35,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${krypton.variable} ${inter.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
