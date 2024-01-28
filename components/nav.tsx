e198e6ded4037d16a0c5449cc5d405f6026d87ee
parisosuch-dev <parisosuch.dev@gmail.com>
2024-01-28 12:36:31 -0800

    Create an account page done.

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,19 +1,22 @@
+'use client';
+
 import Link from "next/link";
 
 export default function Nav() {
+
   return (
     <div className="w-full bg-slate-950 text-white p-8 flex flex-row justify-between">
-      <div>
+      <div className="w-1/2">
         <h1 className="tracking-widest font-black font-krypton text-4xl">
           HYPRCRD
         </h1>
       </div>
-      <div className="space-x-4">
-        <Link href="/sign-up" className="">
-          Sign up
+      <div className="w-1/2 space-x-6 flex justify-end items-center text-center">
+        <Link href="/sign-in" className="hover:underline">
+          Sign In
         </Link>
-        <Link href="/sign-in" className="">
-          Sign in
+        <Link href="/sign-up" className="hover:underline">
+          Create Account
         </Link>
       </div>
     </div>
