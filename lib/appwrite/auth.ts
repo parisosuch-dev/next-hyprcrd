eee4d0732cf21683fceb945c6fbfbe83d4632c0b
parisosuch-dev <parisosuch.dev@gmail.com>
2024-02-22 19:18:38 -0800

    Add comment explaining function

--- lib/appwrite/auth.ts
+++ lib/appwrite/auth.ts
@@ -1,6 +1,9 @@
 import { account } from "@/lib/appwrite/client";
 import { ID } from "@/lib/appwrite/client";
 
+// maybe there will be more functionality with this because
+//  it seems like having a single line function is bad abstraction
+//  maybe i am predicting scale lol
 export async function SignUp(email: string, password: string, name: string) {
   const promise = account.create(ID.unique(), email, password, name);
 
